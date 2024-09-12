9a92b0285a2c72bab24fbb33888f78af9b2a7688
Julia Arukakkal <124928489+juaruk@users.noreply.github.com>
2024-09-11 22:50:42 -0500

    Updated photo gallery for Service Page

--- photo_gallery.js
+++ photo_gallery.js
@@ -1,8 +1,20 @@
 // Array of image data (paths, alt text, and captions)
 const images = [
-    { src: 'Landing_Page.png', alt: 'Photo 1', caption: 'A Beautiful Moment' },
-    { src: 'Landing_Page_2.png', alt: 'Photo 2', caption: 'Memories to Cherish' },
-    { src: 'dkd_headshot.png', alt: 'Photo 3', caption: 'Good Times' }
+    { src: 'Volunteering/48985042-c33b-4e2f-8317-b5ced45414bc.JPG', alt: 'Photo 1', caption: 'Planning dance fundraiser for juvenile diabetes' },
+    { src: 'Volunteering/be417e6f-c252-41ad-b466-f3c195519dba.JPG', alt: 'Photo 2', caption: 'Raised $10,000 for juvenile diabetes' },
+    { src: 'Volunteering/IMG_9023.JPG', alt: 'Photo 3', caption: 'Feed My Starving Childen Big Pack Frisco' },
+    { src: 'Volunteering/IMG_9194.jpg', alt: 'Photo 3', caption: 'TAMU Gardens - Making Cotton' },
+    { src: 'Volunteering/IMG_9196.jpg', alt: 'Photo 3', caption: 'Student Research Week Volunteer' },
+    { src: 'Volunteering/IMG_9197.jpg', alt: 'Photo 3', caption: 'Played Piano Recordings & Recitals for Nursing Homes' },
+    { src: 'Volunteering/IMG_9198.jpg', alt: 'Photo 3', caption: 'Played Clarinet fo Nursing Homes' },
+    { src: 'Volunteering/IMG_9199.jpg', alt: 'Photo 3', caption: 'Designed Flyer for Child Abuse Awareness Event' },
+    { src: 'Volunteering/IMG_9201.jpg', alt: 'Photo 3', caption: 'Peer Notetaker' },
+    { src: 'Volunteering/IMG_9202.JPG', alt: 'Photo 3', caption: 'Made Blankets for Project Linus' },
+    { src: 'Volunteering/IMG_9203.jpg', alt: 'Photo 3', caption: 'Helped Educate Children Globally with STEM articles' },
+    { src: 'Volunteering/IMG_9204.jpg', alt: 'Photo 3', caption: 'Made Bookmarks for Children in Hospitals' },
+    { src: 'Volunteering/IMG_9205.jpg', alt: 'Photo 3', caption: 'Decorated Boys and Girls Club Library for Christmas' },
+    { src: 'Volunteering/IMG_9206.jpg', alt: 'Photo 3', caption: 'More Decorations Boys & Girls Club Christmas' },
+    { src: 'Volunteering/IMG_9207.jpg', alt: 'Photo 3', caption: 'Educated Children' }
   ];
   
   // Select the gallery element by its ID
